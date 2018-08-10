60246c8f09c5ef1e407ece1b0106ef2fa182398f
yasmin <yasmin@readybytes.in>
2018-08-10 16:20:18 +0530

    adding-db-credentials-2

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'https://www.db4free.net'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'db_cities'),
             'username' => env('DB_USERNAME', 'root_cities'),
